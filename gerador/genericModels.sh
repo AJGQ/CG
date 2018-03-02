769044485bab521c725e98b6fea5afd14dad37fa
Artur Jorge <artur11jorge@gmail.com>
2018-03-02 20:06:54 +0000

    faltava o ./

--- gerador/genericModels.sh
+++ gerador/genericModels.sh
@@ -1,15 +1,15 @@
 echo "plane"
-generator plane models/plane.3d
+./generator plane models/plane.3d
 
 echo "box"
-generator box       models/box.3d 3 4 5 10
+./generator box       models/box.3d 3 4 5 10
 
 echo "cylinder"
-generator cylinder  models/cylinder.3d 3 5 24
+./generator cylinder  models/cylinder.3d 3 5 24
 
 echo "cone"
-generator cone      models/cone.3d 3 5 24 10
+./generator cone      models/cone.3d 3 5 24 10
 
 echo "sphere"
-generator sphere    models/sphere.3d 2 24 24
+./generator sphere    models/sphere.3d 2 24 24
 
