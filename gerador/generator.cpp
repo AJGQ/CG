795d1d89acae9c01a780001e1e70e5028a2de325
a73233 <luisneto95@gmail.com>
2018-03-04 15:19:24 +0000

    Foi passado a ordem dos argumentos do generator para a ordem correta: "./generator sphere sphere.3d 1 10 10" --> "./generator sphere 1 10 10 sphere.3d"

--- gerador/generator.cpp
+++ gerador/generator.cpp
@@ -17,14 +17,14 @@ int main(int argc, char** argv){
     vector<Vertex*> vv;
 
     if      ( !strncmp("plane"   , argv[1], 5) && argc==3 ) { createPlane(&vv); }
-    else if ( !strncmp("box"     , argv[1], 3) && argc==7 ) { createBox(&vv, atof(argv[3]), atof(argv[4]), atof(argv[5]), atoi(argv[6])); }
-    else if ( !strncmp("sphere"  , argv[1], 6) && argc==6 ) { createSphere(&vv, atof(argv[3]), atoi(argv[4]), atoi(argv[5])); }
-    else if ( !strncmp("cone"    , argv[1], 4) && argc==7 ) { createCone(&vv, atof(argv[3]), atof(argv[4]), atoi(argv[5]), atoi(argv[6])); }
-    else if ( !strncmp("cylinder", argv[1], 4) && argc==6 ) { createCylinder(&vv, atof(argv[3]), atof(argv[4]), atoi(argv[5])); }
+    else if ( !strncmp("box"     , argv[1], 3) && argc==7 ) { createBox(&vv, atof(argv[2]), atof(argv[3]), atof(argv[4]), atoi(argv[5])); }
+    else if ( !strncmp("sphere"  , argv[1], 6) && argc==6 ) { createSphere(&vv, atof(argv[2]), atoi(argv[3]), atoi(argv[4])); }
+    else if ( !strncmp("cone"    , argv[1], 4) && argc==7 ) { createCone(&vv, atof(argv[2]), atof(argv[3]), atoi(argv[4]), atoi(argv[5])); }
+    else if ( !strncmp("cylinder", argv[1], 4) && argc==6 ) { createCylinder(&vv, atof(argv[2]), atof(argv[3]), atoi(argv[4])); }
 
     else cout << "\e[1;31mError:\e[0;1m try man generator\e[0m" << endl;
 
-    ofstream file(argv[2]);
+    ofstream file(argv[argc-1]);
     for_each(vv.begin(), vv.end(), [&file](Vertex* v) { file << v->toString(); });
 
     return 0;
