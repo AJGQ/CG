d8e55ecaedc4e50aa8e33c0890db0567c0bc5802
Unknown <adriano.vc.campinho@gmail.com>
2018-03-07 17:50:02 +0000

    faltava isto

--- engine/parseXML.cpp
+++ engine/parseXML.cpp
@@ -7,7 +7,7 @@ extern vector<const char*> fileNameModels;
 
 //-----------------------------------------------------------------------------//
 
-void drawFile(const char* filePath){
+void drawFile(const char* filePath) {
 
     FILE* file;
     int i, cont, nbuff, *pos, *tot;
@@ -24,7 +24,7 @@ void drawFile(const char* filePath){
     tot = (int*)malloc(nbuff * sizeof(int));
     typ = (char*)malloc(nbuff * sizeof(char));
 
-    for(i=0, cont=0; i<nbuff; i++){
+    for(i=0, cont=0; i<nbuff; i++) {
         fscanf(file,"%c%d\n", &typ[i], &tot[i]);
         pos[i] = cont;
         cont += tot[i];
@@ -34,7 +34,7 @@ void drawFile(const char* filePath){
     glGenBuffers(1, buffers);
     arrayFloat = (float *)(malloc(cont * 3 * sizeof(float)));
 
-    for(i=0; fscanf(file,"%f:%f:%f\n",&x,&y,&z) != EOF; i+=3){
+    for(i=0; fscanf(file,"%f:%f:%f\n",&x,&y,&z) != EOF; i+=3) {
 //fprintf(stderr,"%f:%f:%f\n", x, y, z);
         arrayFloat[i] = x;
         arrayFloat[i+1] = y;
@@ -48,10 +48,10 @@ void drawFile(const char* filePath){
 
 	  glVertexPointer(3,GL_FLOAT,0,0);
 
-    for(i=0; i<nbuff; i++){
+    for(i=0; i<nbuff; i++) {
     //fprintf(stderr,"%c\n", typ[i]);
     //fprintf(stderr,"%d:%d\n", pos[i], tot[i]);
-        switch(typ[i]){
+        switch(typ[i]) {
             case 's' : glDrawArrays(GL_TRIANGLE_STRIP   , pos[i], tot[i]); break;
             case 'f' : glDrawArrays(GL_TRIANGLE_FAN     , pos[i], tot[i]); break;
             case 't' : glDrawArrays(GL_TRIANGLES        , pos[i], tot[i]); break;
@@ -71,7 +71,7 @@ void drawFiles() {
 
 //-----------------------------------------------------------------------------//
 
-void parseXML(char *fileXML){
+void parseXML(char *fileXML) {
     int i, size, len;
     xml_document doc;
     xml_parse_result result;
@@ -79,8 +79,8 @@ void parseXML(char *fileXML){
     if( !(result = doc.load_file(fileXML)) ) error( result.description() );
     xml_node models = doc.child("scene");
 
-    for(xml_node model = models.first_child(); model; model = model.next_sibling()){
-        for(xml_attribute attr = model.first_attribute(); attr; attr = attr.next_attribute()){
+    for(xml_node model = models.first_child(); model; model = model.next_sibling()) {
+        for(xml_attribute attr = model.first_attribute(); attr; attr = attr.next_attribute()) {
             len = strlen(attr.value())+1;
             char *model = (char*)malloc(len);
             strncpy(model, attr.value(), len);
